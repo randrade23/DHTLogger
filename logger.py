cfd4debbd217bd895c10db44bbf0d9e0082c342a
Rui Andrade <ruiandrade@outlook.com>
2017-06-12 14:46:17 +0100

    testing

--- logger.py
+++ logger.py
@@ -8,7 +8,7 @@ conn = MySQLdb.connect(host= "localhost",
                   passwd=secret.pwd(),
                   db=secret.db())
 cursor = conn.cursor()
-query = "INSERT INTO dht(epoch, temperature, humidity, heatindex) VALUES(%d,%f,%f,%f)"
+query = "INSERT INTO dht(epoch, temperature, humidity, heatindex) VALUES(:1,:1,:1,:1)"
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
@@ -16,4 +16,4 @@ while True:
     line = ser.readline()
     spl = line.split()
     epoch_time = int(time.time())
-    cursor.execute(query, [int(epoch_time), float(spl[0]), float(spl[1]), float(spl[2])])
+    cursor.execute(query, [str(epoch_time), spl[0], spl[1], spl[2]])
