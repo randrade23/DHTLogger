236c7dd8ba2c19c878dcb545aa3010355ab9a24c
Rui Andrade <ruiandrade@outlook.com>
2017-06-12 14:55:24 +0100

    fixing query building

--- logger.py
+++ logger.py
@@ -8,12 +8,16 @@ conn = MySQLdb.connect(host= "localhost",
                   passwd=secret.pwd(),
                   db=secret.db())
 cursor = conn.cursor()
-query = "INSERT INTO dht(epoch, temperature, humidity, heatindex) VALUES(:1,:1,:1,:1)"
+query = "INSERT INTO dht(epoch, temperature, humidity, heatindex) VALUES(%d, %.2f, %.2f, %.2f)"
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
     line = ser.readline()
     spl = line.split()
+    print spl
     epoch_time = int(time.time())
-    cursor.execute(query, [str(epoch_time), spl[0], spl[1], spl[2]])
+    cursor.execute(query % (epoch_time, float(spl[0]), float(spl[1]), float(spl[2]))
+   	conn.commit()
+
+conn.close()
