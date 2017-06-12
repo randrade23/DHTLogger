f350b239f1a9757ec8921fd8711db8dd14e2b275
Rui Andrade <ruiandrade@outlook.com>
2017-06-12 15:03:38 +0100

    types again

--- tables.sql
+++ tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE dht (
 	epoch int(11) not null,
-	humidity float(2,2) not null,
-	temperature float(2,2) not null,
-	heatindex float(2,2) not null
+	humidity decimal(4,2) not null,
+	temperature decimal(4,2) not null,
+	heatindex decimal(4,2) not null
 );
